4a9bbb59062583fdd56eb0b3906b5075d6913d68
Brian Dear <superacidjax@me.com>
2013-03-12 16:03:56 -0500

    Adds automatic spots available updating based on tickets sold

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :description, :price, :title
+  attr_accessible :date, :description, :price,
+                  :title, :spots_available, :tickets_sold
+
+  before_save :update_spots_available
+
+  def update_spots_available
+    if self.spots_available
+      self.spots_available = self.spots_available - self.tickets_sold
+    else
+    end
+  end
+
 end
