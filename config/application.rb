4ec28f1ea277b29c6ac5804ca7c925db66357b59
Brian Dear <superacidjax@me.com>
2013-03-12 16:38:17 -0500

    disables precomp for Heroku

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,8 @@ module Pourpedal
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
