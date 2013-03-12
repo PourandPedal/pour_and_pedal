1fdea9518665ba3183878be5543c0006f457c965
Brian Dear <superacidjax@me.com>
2013-03-12 09:59:48 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+
+gem 'hashrocket-rails'
+gem 'jquery-rails'
+gem 'sorcery'
+gem 'thin'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  # gem 'database_cleaner'
+  # gem 'faker'
+  # gem 'launchy'
+  gem 'pry-debugger'
+  gem 'pry-rails'
+  # gem 'rspec-rails', '~> 2.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pourpedal::Application.config.secret_token = '9bd2e85948cdc1a08c9cb440e4c858531fc09ec1dfe369ea4b45eff7c5067e4e0f936c415cc3ab56ef7ecac33d54cf8e9dfc2d5148eaaa4f6a7529ad964c6808'
