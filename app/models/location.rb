58fc21fd9f386a20dd072220ce2ea0a182782549
Brian Dear <superacidjax@me.com>
2013-03-12 16:18:41 -0500

    Adds description to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :name, :photos_attributes
+  attr_accessible :name, :photos_attributes, :description
   has_many :photos, dependent: :destroy, inverse_of: :location
   accepts_nested_attributes_for :photos, allow_destroy: true
 end
