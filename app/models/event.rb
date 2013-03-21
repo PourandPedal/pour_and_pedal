f530fa806afc925a7b4b98f6c7515cfa4b5ef134
Brian Dear <superacidjax@me.com>
2013-03-21 13:55:38 -0600

    Configures event model and pages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,18 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :description, :price,
-                  :title, :spots_available, :tickets_sold
+                  :title, :spots_available, :tickets_sold, :location,
+                  :directions, :location_id
 
-  before_save :update_spots_available
+  # before_save :update_spots_available
+  has_many :confirmations
+  has_many :clients, through: :confirmations
+  belongs_to :location
 
-  def update_spots_available
-    if self.spots_available
-      self.spots_available = self.spots_available - self.tickets_sold
-    else
-    end
-  end
+  # def update_spots_available
+  #   if self.spots_available
+  #     self.spots_available = self.spots_available - self.tickets_sold
+  #   else
+  #   end
+  # end
 
 end
