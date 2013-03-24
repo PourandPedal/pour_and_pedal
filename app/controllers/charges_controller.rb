618f9da9ed7c57f60ee216f65d9544b24a36c050
Brian Dear <superacidjax@me.com>
2013-03-24 09:03:54 -0600

    Refactors charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,32 +6,31 @@ class ChargesController < ApplicationController
     session[:quantity] = @quantity
     new_client
     session[:event] = @event
-    @redemption_code = Confirmation.find_by_confirmation_number(params[:redemption_code])
-    session[:event_price] = (@event.price - @redemption_code.value) unless
-      (@redemption_code.blank? || @redemption_code.is_expired?)
-    if @redemption_code && @redemption_code.is_expired?
-      session[:redemption_code_expired] = true
+
+    if !params[:redemption_code].blank?
+      @redemption_code = Confirmation.find_by_confirmation_number(params[:redemption_code])
+      set_event_price_with_code if @redemption_code
+      session[:redemption_code] = @redemption_code
     else
-      session[:redemption_code_expired] = nil
+      session[:event_price] = @event.price
     end
-    @event.price = session[:event_price] unless @redemption_code.blank?
-    session[:redemption_code] = @redemption_code
+  end
+
+  def set_event_price_with_code
+    session[:event_price] = (@event.price - @redemption_code.value) unless @redemption_code.is_expired?
   end
 
   def create
     if session[:event_price] && session[:event_price] <= 0
       create_with_no_balance_due
     else
-      if session[:event_price] && session[:event_price]
-        @event.price = session[:event_price]
-      else
-      end
+      @this_event_price = session[:event_price]
       @newclient = session[:new_client]
       @event = session[:event]
       @quantity = session[:quantity]
 
       # Amount in cents
-      @amount = (@event.price * 100 * @quantity)
+      @amount = (@this_event_price * 100 * @quantity)
       @amount = @amount.to_i
 
 
@@ -65,10 +64,9 @@ class ChargesController < ApplicationController
       session[:event_price] = nil
     end
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path
-
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to charges_path
   end
 
   def create_with_no_balance_due
@@ -119,15 +117,4 @@ class ChargesController < ApplicationController
     confirmation_number = (rand(1000000) + 2013).to_s + "e" + event.id.to_s +
                           "c" + client.id.to_s
   end
-
-  # def redeem
-  #   binding.pry
-  #   if !params[:redemption_code].blank?
-  #     @redemption = Confirmation.find_by_confirmation_number(params[:redemption_code])
-  #   else
-  #     binding.pry
-  #   end
-  # end
-
-
 end
