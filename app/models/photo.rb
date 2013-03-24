41655f9dabaf17fd2be41b87512bad24612677f0
Brian Dear <superacidjax@me.com>
2013-03-24 11:06:28 -0600

    Ensures only one primary photo at a time

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,25 @@ class Photo < ActiveRecord::Base
   belongs_to :location, inverse_of: :photos
   belongs_to :event, inverse_of: :photos
   mount_uploader :photo, PhotoUploader
+
+  before_save :set_primary
+
+  def set_primary
+    if self.is_primary?
+      current_primary = self
+      if self.event_id
+        photos = Photo.where(location_id: self.location_id, event_id: self.event_id, is_primary: true)
+      else
+        photos = Photo.where(location_id: self.location_id, is_primary: true)
+      end
+        photos.each do |photo|
+          if photo == current_primary
+          else
+            photo.is_primary = false
+            photo.save
+          end
+        end
+    else
+    end
+  end
 end
