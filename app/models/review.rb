eecb98fea749668fa2f801f97dd989137cf0fa41
Brian Dear <superacidjax@me.com>
2013-03-30 18:21:20 -0400

    Handles for nil hometown on Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :hometown, presence: true, length: { maximum: 50 }
+  # validates :hometown, presence: true, length: { maximum: 50 }
   validates :content, presence: true
 
 end
