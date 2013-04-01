6d0b0958ad324fb4f81519c7a0b4cf52175df2b7
Brian Dear <superacidjax@me.com>
2013-04-01 16:32:41 -0600

    changes "trip" to "event" for consistency in views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
 
   def create
     if contact.save
-      redirect_to events_path, notice: 'message sent. how about browsing some of our great trips!'
+      redirect_to events_path, notice: 'message sent. how about browsing some of our great events!'
       NotificationMailer.new_contact(contact).deliver
     else
       render :new
