9de7ed30592cff977cac1cab7e385d91170aff1b
Brian Dear <superacidjax@me.com>
2013-04-01 16:38:07 -0600

    Adds validations for Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,5 +4,8 @@ class Trip < ActiveRecord::Base
   has_many :confirmations
   has_many :clients, through: :confirmations
   belongs_to :event, inverse_of: :trips
+
+  validates_presence_of :title, :date, :price
+
   has_paper_trail
 end
