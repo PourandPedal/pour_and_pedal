f0b67bc710a1e8ebaf46a436882b1b9422e291aa
Brian Dear <superacidjax@me.com>
2013-04-02 13:26:57 -0600

    compiles gallery css and coffee

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,7 @@ module Pourpedal
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    config.assets.precompile += %w( html5.js )
+    config.assets.precompile += %w( 'html5.js', 'gallery.css', 'gallery.js' )
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
