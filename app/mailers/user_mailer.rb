c530c857e175b704f0ad6fb7352fb13906433008
Brian Dear <superacidjax@me.com>
2013-04-01 18:55:39 -0600

    disables environment-specific urls

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "brian@icouch.me"
 
 
 
@@ -14,7 +14,8 @@ class UserMailer < ActionMailer::Base
 
   def reset_password_email(user)
     @user = user
-    environment_url(@user)
+    @url = "http://pour-and-pedal.herokuapp.com/password_resets/#{user.reset_password_token}/edit"
+    # environment_url(@user)
     mail(:to => user.email,
          :subject => "P&P: Your password has been reset")
   end
