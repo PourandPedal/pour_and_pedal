9f8955779e28de626268c9d029381a338be68493
Brian Dear <superacidjax@me.com>
2013-04-13 13:53:29 -0500

    Removes old code

--- app/controllers/gift_certificates_controller.rb
+++ app/controllers/gift_certificates_controller.rb
@@ -3,10 +3,6 @@ class GiftCertificatesController < ApplicationController
   expose(:gift_certificate)
   expose(:client)
 
-  # def new
-  #   @client = gift_certificate.client.build
-  # end
-
   def create
     client = Client.create(params[:client])
     session[:client_id] = client.id
@@ -24,45 +20,4 @@ class GiftCertificatesController < ApplicationController
     session[:price] = @cert.number_purchased * 125
   end
 
-  # def gift_certificates_charge
-  # end
-    # cert.
-
-  # def gift_certificate_charge
-
-  #   @amount = (@this_trip_price * 100)
-  #   @amount = @amount.to_i
-
-
-  #     customer = Stripe::Customer.create(
-  #       :email => @newclient.email,
-  #       :card  => params[:stripeToken]
-  #     )
-
-  #     charge = Stripe::Charge.create(
-  #       :customer    => customer.id,
-  #       :amount      => @amount,
-  #       :description => @trip.event.title,
-  #       :currency    => 'usd'
-  #     )
-
-  #     @newclient.stripe_id = customer.id
-  #     @newclient.trip_id = @trip.id
-  #     @newclient.amount_paid = (@amount/100)
-  #     @newclient.tickets_purchased = @quantity
-  #     @newclient.date_paid = Date.today
-  #     @newclient.save
-  #     @confirmation_number = generate_confirmation_number(@trip, @newclient)
-  #     confirmation_create(@confirmation_number, @newclient, @trip, @quantity)
-  #     used_code if session[:redemption_code].present?
-
-  #     NotificationMailer.booking_notification(@trip).deliver
-  #     NotificationMailer.booking_confirmation(@trip, @newclient).deliver unless @newclient.email.blank?
-  #   end
-
-  #   rescue Stripe::CardError => e
-  #     flash[:error] = e.message
-  #     redirect_to charges_path
-  # end
-  # end
 end
