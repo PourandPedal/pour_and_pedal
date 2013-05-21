067e2cfe903591b8113acb723ed8ac6a59da04c4
Brian Dear <superacidjax@me.com>
2013-05-21 00:17:57 -0400

    adds google site verification

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,23 @@ class TripsController < ApplicationController
 
   layout false
 
+
+
   expose(:trip)
   expose(:trips)
+
+  def show
+    if current_user && current_user.admin?
+    else
+      redirect_to root_path, notice: 'You must be logged in to see that page!'
+    end
+  end
+
+  def index
+    if current_user && current_user.admin?
+    else
+      redirect_to root_path, notice: 'You must be logged in to see page!!'
+    end
+  end
 
 end
